4888fd99395624f06555a6eb4957bb7f50ddd242
MicoPham <michaelxpham@gmail.com>
2021-06-17 23:46:47 -0700

    Complete calculator assignment

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,95 @@
+let a = '';
+let b = '';
+let operator = '';
+
+const numsBtn = document.querySelectorAll('.num');
+const display = document.querySelector('.display');
+const clearBtn = document.querySelector('.clear');
+const operatorBtn = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
+
+equalBtn.addEventListener('click', (e) => {
+    if (a !== '' && b !== '' && operator !== '') {
+        let temp = operate(operator, Number(a), Number(b));
+        display.textContent = temp;
+        a = temp.toString();
+        b = '';
+        operator = '';
+        return;
+    }
+})
+
+clearBtn.addEventListener('click', (e) => {
+    clear();    
+})
+
+numsBtn.forEach((num) => {
+    num.addEventListener('click', (e) => {
+        if (a === '' && (b === '' || b !== '') && operator === ''){
+            display.textContent += e.target.innerText;
+            b = display.textContent;
+            console.log(b);
+            return;
+        } 
+        if (a !== '' && b === '' && operator !== '' ){
+            display.textContent = e.target.innerText;
+            b = display.textContent;
+            console.log(b);
+            return;
+            } 
+        if (a !== '' && b !== '' && operator !== '' ){
+            display.textContent += e.target.innerText;
+            b = display.textContent;
+            console.log(b);
+            return;
+            }
+        if (a !== '' && b === '' && operator === '' ){
+            display.textContent = e.target.innerText;
+            b = display.textContent;
+            console.log(b);
+            return;
+            } 
+        if (a !== '' && b !== '' && operator === '' ){
+            display.textContent += e.target.innerText;
+            b = display.textContent;
+            console.log(b);
+            return;
+            } 
+    });
+});
+
+
+operatorBtn.forEach((oper) => {
+    oper.addEventListener('click', (e) => {
+        if (a === '' && b === ''){
+            return
+        } 
+        if (a === '' && b !== '' && operator === ''){
+            a = b;
+            b = '';
+            display.textContent = '';
+            operator = e.target.innerText;
+            console.log(operator);
+            return
+        }
+        if (a !== '' && b === '' && operator === '') {
+            operator = e.target.innerText;
+            display.textContent = '';
+            console.log(operator);
+            return;
+        }
+        if (a !== '' && b !== '' && operator !== '') {
+            let temp = operate(operator, Number(a), Number(b));
+            display.textContent = temp;
+            a = temp.toString();
+            b = '';
+            operator = e.target.innerText;
+            return;
+        }
+        }
+    );
+});
+
 function add(a, b) {
     return a + b;
 }
@@ -31,5 +123,12 @@ function operate(operator, a, b) {
         default:
             return
     }
+}
+
+function clear(){
+    a = '';
+    b = '';
+    operator = '';
+    display.textContent = '';
 }
 
